ce86825fe1c6e767b9f3538f419d4ac74c835678
yeana0324 <yeana.dev@gmail.com>
2021-10-18 16:46:04 -0500

    Added background gradient

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import "./App.css";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Home />
+        <About />
+        <Projects />
       </Layout>
     </div>
   );
