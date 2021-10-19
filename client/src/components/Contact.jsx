57d47d627043432595221f4811a5e672944d65be
yeana0324 <yeana.dev@gmail.com>
2021-10-19 17:14:25 -0500

    Added container to make the code eaiser to style

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -27,40 +27,43 @@ export default function Contact() {
 
   return (
     <div id="contact">
-      <div id="contact-left">
-        <header>💌 Contact me!</header>
-        <p>
-          I would love to have a chat with you! So fill in your info in the
-          form, and let's build something creative! Feel free to look at my
-          resume, GitHub, and LinkedIn 😊 Looking forward to speaking with you!
-        </p>
-        <div id="about-links">
-          <a
-            href="https://drive.google.com/file/d/13QWoYpUSJ78O2bbw-zrunp_qmQq6-FBq/view?usp=sharing"
-            target="_blank"
-          >
-            <button>
-              <i className="fas fa-file" /> Resume
-            </button>
-          </a>
-          <a href="https://github.com/yeana-dev" target="_blank">
-            <button>
-              <i className="fab fa-github-square" /> Github
-            </button>
-          </a>
-          <a href="https://www.linkedin.com/in/yeanacho/" target="_blank">
-            <button>
-              <i className="fab fa-linkedin" /> LinkedIn
-            </button>
-          </a>
+      <div id="container">
+        <div id="contact-left">
+          <header>💌 Contact me!</header>
+          <p>
+            I would love to have a chat with you! So fill in your info in the
+            form, and let's build something creative! Feel free to look at my
+            resume, GitHub, and LinkedIn 😊 Looking forward to speaking with
+            you!
+          </p>
+          <div id="about-links">
+            <a
+              href="https://drive.google.com/file/d/13QWoYpUSJ78O2bbw-zrunp_qmQq6-FBq/view?usp=sharing"
+              target="_blank"
+            >
+              <button>
+                <i className="fas fa-file" /> Resume
+              </button>
+            </a>
+            <a href="https://github.com/yeana-dev" target="_blank">
+              <button>
+                <i className="fab fa-github-square" /> Github
+              </button>
+            </a>
+            <a href="https://www.linkedin.com/in/yeanacho/" target="_blank">
+              <button>
+                <i className="fab fa-linkedin" /> LinkedIn
+              </button>
+            </a>
+          </div>
         </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Your name" name="user_name" />
+          <input type="email" placeholder="Email" name="user_email" />
+          <textarea name="message" placeholder="Message" rows="6" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
-      <form ref={form} onSubmit={sendEmail}>
-        <input type="text" placeholder="Your name" name="user_name" />
-        <input type="email" placeholder="Email" name="user_email" />
-        <textarea name="message" placeholder="Message" rows="6" />
-        <input type="submit" value="Send" />
-      </form>
     </div>
   );
 }
