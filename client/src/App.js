df7fbf36114a52b327e42903d4079f5ad8b109b9
yeana0324 <yeana.dev@gmail.com>
2021-10-19 15:41:59 -0500

    Added contact screen

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </Layout>
     </div>
   );
