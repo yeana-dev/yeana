b6b6a7a15ba38b10f3c8290f19b7daa3c8ec6e0e
yeana0324 <yeana.dev@gmail.com>
2021-10-19 17:14:13 -0500

    Added container to make the code eaiser to style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -21,31 +21,33 @@ export default function Home() {
   }, []);
 
   return (
-    <div id="home" className="container">
-      <div className="home-left">
-        <header>
-          {greeting} 👋&nbsp;&nbsp;My name is<span id="yeana">Yeana Cho</span>
-        </header>
-        <div className="short-description">
-          I am a <span className="highlight">software engineer</span> born in
-          Seoul, South Korea 🇰🇷, <br />
-          and currently based in Houston, Texas 🇺🇸
-          <p>
-            I enjoy building{" "}
-            <Typewriter
-              words={[
-                "interactive applications.",
-                "eye-catching applications.",
-                "clean and well-strctured codebase.",
-              ]}
-              typingSpeed="50"
-              erasingSpeed="60"
-            />
-          </p>
+    <div id="home">
+      <div className="container">
+        <div className="home-left">
+          <header>
+            {greeting} 👋&nbsp;&nbsp;My name is<span id="yeana">Yeana Cho</span>
+          </header>
+          <div className="short-description">
+            I am a <span className="highlight">software engineer</span> born in
+            Seoul, South Korea 🇰🇷, <br />
+            and currently based in Houston, Texas 🇺🇸
+            <p>
+              I enjoy building{" "}
+              <Typewriter
+                words={[
+                  "interactive applications.",
+                  "eye-catching applications.",
+                  "clean and well-strctured codebase.",
+                ]}
+                typingSpeed="50"
+                erasingSpeed="60"
+              />
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="home-right">
-        <MiniAvatar />
+        <div className="home-right">
+          <MiniAvatar />
+        </div>
       </div>
     </div>
   );
