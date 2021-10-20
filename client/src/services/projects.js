74fbe35914890ffd455ec5ef0fc96c7c2fe58b6a
yeana0324 <yeana.dev@gmail.com>
2021-10-20 13:47:46 -0500

    Modified html elements to use html-react-parser on screens

--- client/src/services/projects.js
+++ client/src/services/projects.js
@@ -9,15 +9,7 @@ export const projects = [
     preview: { WitmePreview },
     sub_header: "A collaborative platform for devs and designers",
     description: "A Full-stack application using PostgreSQL and Ruby on Rails.",
-    features: `Features: WYSIWYG text editor ${(
-      <a
-        href="https://ui.toast.com/tui-editor"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Toast UI Editor
-      </a>
-    )} , comments, login/signup using JWT and Bcrypt`,
+    features: `Features: WYSIWYG text editor <a href="https://ui.toast.com/tui-editor" target="_blank" rel="noreferrer">Toast UI Editor</a>, comments, login/signup using JWT and Bcrypt`,
     used_stacks: [
       "HTML",
       "CSS",
@@ -38,11 +30,7 @@ export const projects = [
     sub_header: "E-commerce for a virtual eyewear boutique",
     description:
       "A full-stack MERN application built with three other developers using MongoDB and Express.js. Used JWT and bcrypt for user authentication and authorization.",
-    features: `Glasses' image datas are from ${(
-      <a href="https://www.warbyparker.com/" target="_blank" rel="noreferrer">
-        Warby Parker
-      </a>
-    )}`,
+    features: `Glasses' image datas are from <a href="https://www.warbyparker.com/" target="_blank" rel="noreferrer">Warby Parker</a>`,
     used_stacks: [
       "HTML",
       "CSS",
@@ -63,11 +51,7 @@ export const projects = [
     sub_header: "Developer’s Log",
     description:
       "Single-page application for developers to log their learning progress and post their projects. Posts are categorized by languages and framework.",
-    features: `Used ${(
-      <a href="https://www.airtable.com/" target="_blank" rel="noreferrer">
-        Airtable RESTful API{" "}
-      </a>
-    )}`,
+    features: `Used <a href="https://www.airtable.com/" target="_blank" rel="noreferrer">Airtable RESTful API</a>`,
     used_stacks: [
       "HTML",
       "CSS",
@@ -85,11 +69,7 @@ export const projects = [
     sub_header: "Nintendo Switch game 'Animal Crossing' critter encyclopedia",
     description:
       "Pure JavaScript application for users to search critters by many search methods. User can insert time slot to see which critters are available, or simply search by their name.",
-    features: `Used ${(
-      <a href="http://acnhapi.com/" target="_blank" rel="noreferrer">
-        ACNH RESTful API
-      </a>
-    )}`,
+    features: `Used <a href="http://acnhapi.com/" target="_blank" rel="noreferrer">ACNH RESTful API</a>`,
     used_stacks: ["HTML", "CSS", "JavaScript"],
     siteURL: "https://critterdex.netlify.app",
     githubURL: "https://github.com/yeana-dev/Critterdex-for-animal-crossing",
