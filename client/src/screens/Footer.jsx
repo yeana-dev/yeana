a1491467743b2818cf2979d46fa9048816412c30
yeana0324 <yeana.dev@gmail.com>
2021-10-20 13:48:10 -0500

    Modified footer text and added top button

--- client/src/screens/Footer.jsx
+++ client/src/screens/Footer.jsx
@@ -1,5 +1,17 @@
 import "./style/Footer.css";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="footer-left">
+        2021 | Designed and coded by
+        <a href="https://www.linkedin.com/in/yeanacho/">Yeana Cho</a>
+      </div>
+      <div className="top-arrow">
+        <a href="#home">
+          <i className="far fa-arrow-alt-circle-up" />
+        </a>
+      </div>
+    </footer>
+  );
 }
