19f32ea82e0f763c814939c1c5498cfc6072bc76
yeana0324 <yeana.dev@gmail.com>
2021-12-04 21:10:17 -0600

    Created custom color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      purple: colors.violet,
+    },
   },
   variants: {
     extend: {},
