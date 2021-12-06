b3bc89eda2809759036cbc62127f89519601c9f6
yeana0324 <yeana.dev@gmail.com>
2021-12-05 22:35:57 -0600

    Added new projects

--- client/src/services/projects.js
+++ client/src/services/projects.js
@@ -1,12 +1,32 @@
+// Main Preview
+import AaajPreview from "../assets/aaaj_preview.png";
 import WitmePreview from "../assets/witme_preview.png";
 import YsfjPreview from "../assets/ysfj_preview.png";
 import DevlogPreview from "../assets/devlog_preview.png";
 import CritterdexPreview from "../assets/critterdex_preview.png";
+import WeatherappPreview from "../assets/weatherapp_preview.png";
+// Prototype
+import WitmePrototype from "../assets/prototype/witme_preview.png";
+import YsfjPrototype from "../assets/prototype/ysfj_preview.png";
+import DevlogPrototype from "../assets/prototype/devlog_preview.png";
+import CritterdexPrototype from "../assets/prototype/critterdex_preview.png";
 
 export const projects = [
   {
+    title: "Asian American Advancing Justice",
+    mainPreview: AaajPreview,
+    // prototype: AaajPrototype,
+    seb_header: "test",
+    description: "test",
+    features: `test`,
+    used_stacks: ["HTML"],
+    siteURL: "https://advancing-justice.netlify.app",
+    githubURL: "https://github.com",
+  },
+  {
     title: "Witme",
-    preview: { WitmePreview },
+    mainPreview: WitmePreview,
+    prototype: WitmePrototype,
     sub_header: "A collaborative platform for devs and designers",
     description: "A Full-stack application using PostgreSQL and Ruby on Rails.",
     features: `Features: WYSIWYG text editor <a href="https://ui.toast.com/tui-editor" target="_blank" rel="noreferrer">Toast UI Editor</a>, comments, login/signup using JWT and Bcrypt`,
@@ -26,7 +46,8 @@ export const projects = [
   },
   {
     title: "YSFJ",
-    preview: { YsfjPreview },
+    mainPreview: YsfjPreview,
+    prototype: YsfjPrototype,
     sub_header: "E-commerce for a virtual eyewear boutique",
     description:
       "A full-stack MERN application built with three other developers using MongoDB and Express.js. Used JWT and bcrypt for user authentication and authorization.",
@@ -47,7 +68,8 @@ export const projects = [
   },
   {
     title: "Devlog",
-    preview: { DevlogPreview },
+    mainPreview: DevlogPreview,
+    prototype: DevlogPrototype,
     sub_header: "Developer’s Log",
     description:
       "Single-page application for developers to log their learning progress and post their projects. Posts are categorized by languages and framework.",
@@ -65,7 +87,8 @@ export const projects = [
   },
   {
     title: "Critterdex",
-    preview: { CritterdexPreview },
+    mainPreview: CritterdexPreview,
+    prototype: CritterdexPrototype,
     sub_header: "Nintendo Switch game 'Animal Crossing' critter encyclopedia",
     description:
       "Pure JavaScript application for users to search critters by many search methods. User can insert time slot to see which critters are available, or simply search by their name.",
@@ -73,5 +96,16 @@ export const projects = [
     used_stacks: ["HTML", "CSS", "JavaScript"],
     siteURL: "https://critterdex.netlify.app",
     githubURL: "https://github.com/yeana-dev/Critterdex-for-animal-crossing",
+  },
+  {
+    title: "Weather App",
+    mainPreview: WeatherappPreview,
+    // prototype: AaajPrototype,
+    seb_header: "test",
+    description: "test",
+    features: `test`,
+    used_stacks: ["HTML"],
+    siteURL: "https://advancing-justice.netlify.app",
+    githubURL: "https://github.com",
   },
 ];
