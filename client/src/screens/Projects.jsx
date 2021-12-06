5670a81842bfa57fd16ae9b5fdb90fabb2838739
yeana0324 <yeana.dev@gmail.com>
2021-12-05 22:36:58 -0600

    Replaced project component to preview images

--- client/src/screens/Projects.jsx
+++ client/src/screens/Projects.jsx
@@ -1,14 +1,14 @@
-import "./style/Projects.css";
-import Project from "../components/Project";
 import { projects } from "../services/projects";
 
 export default function Projects() {
   return (
-    <div id="projects">
-      <div id="container">
-        <header id="projects-header">🖥 Recent projects I built</header>
+    <div className="py-20 " id="projects">
+      <h1 className="font-sans font-bold text-4xl uppercase tracking-tighter text-center bg-split-purple-bottom">
+        🖥 Projects
+      </h1>
+      <div className="flex flex-wrap justify-center gap-16 my-20 max-w-6xl mx-auto">
         {projects.map((project) => (
-          <Project project={project} />
+          <img src={project.mainPreview} />
         ))}
       </div>
     </div>
