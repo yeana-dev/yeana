9c913b1d60c963788556467d160fe13625b2452f
yeana0324 <yeana.dev@gmail.com>
2021-12-11 13:02:18 -0600

    Transferred vanilla CSS to TailwindCSS

--- client/src/screens/Footer.jsx
+++ client/src/screens/Footer.jsx
@@ -1,15 +1,20 @@
-import "./style/Footer.css";
+// import "./style/Footer.css";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="footer-left">
-        2021 | Designed and coded by
-        <a href="https://www.linkedin.com/in/yeanacho/">Yeana Cho</a>
-      </div>
-      <div className="top-arrow">
+    <footer className="bg-gray-200">
+      <div className="max-w-6xl mx-auto p-6 flex justify-between items-center">
+        <div className="font-sans tracking-tighter text-xs text-gray-400">
+          2021 | Designed and coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/yeanacho/"
+            className="font-semibold italic text-gray-700"
+          >
+            Yeana Cho
+          </a>
+        </div>
         <a href="#home">
-          <i className="far fa-arrow-alt-circle-up" />
+          <i className="far fa-arrow-alt-circle-up text-4xl text-gray-400" />
         </a>
       </div>
     </footer>
