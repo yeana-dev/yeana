f58eb1188cc7ec0e4ee1938bfebc14b9a31d57ce
yeana0324 <yeana.dev@gmail.com>
2021-12-10 19:51:50 -0600

    Added feature that automatically scrolls to top when route changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import Layout from "./components/Layout";
 import Home from "./screens/Home";
 import ProjectDetail from "./screens/ProjectDetail";
 import { Routes, Route } from "react-router-dom";
-// import { projects } from "../services/projects";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:slug" element={<ProjectDetail />} />
-        </Routes>
-      </Layout>
+      <ScrollToTop>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:slug" element={<ProjectDetail />} />
+          </Routes>
+        </Layout>
+      </ScrollToTop>
     </div>
   );
 }
